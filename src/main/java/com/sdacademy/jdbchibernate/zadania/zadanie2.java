2a204f13b32e3a4d9a950cbfc9fb8d3b4c055172
Jareczeq <contact@jareczeq.pl>
2020-03-28 12:36:33 +0100

    Poprawa zadania 2

--- src/main/java/com/sdacademy/jdbchibernate/zadania/zadanie2.java
+++ src/main/java/com/sdacademy/jdbchibernate/zadania/zadanie2.java
@@ -13,9 +13,22 @@ import java.sql.SQLException;
 public class zadanie2 {
 
     public static void make() {
-        String query = "UPDATE product SET PRO_DESCRIPTION = \"Brak\" WHERE PRO_ID IN(1, 3, 5)";
+
+        int[] tablica = new int[] {1, 3, 5};
+
+        StringBuilder builder = new StringBuilder("?");
+
+        for (int i=1; i<tablica.length; i++) {
+            builder.append(",?");
+        }
+
+        String query = "UPDATE product SET PRO_DESCRIPTION = CONCAT(PRO_DESCRIPTION, \" \", \"BRAK\") WHERE PRO_ID IN(" + builder.toString() +")";
 
         try (PreparedStatement statement = DatabaseConnection.getInstance().getConnection().prepareStatement(query)) {
+
+            for (int i=0; i<tablica.length; i++) {
+                statement.setInt(i + 1, tablica[i]);
+            }
 
             int countRecords = statement.executeUpdate();
 
