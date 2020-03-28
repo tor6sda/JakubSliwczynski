590ea8c695b0c1d4e7dfa0506d24de5183c7c460
Jareczeq <contact@jareczeq.pl>
2020-03-28 12:41:37 +0100

    Poprawa zadania 2 v2

--- src/main/java/com/sdacademy/jdbchibernate/zadania/zadanie2.java
+++ src/main/java/com/sdacademy/jdbchibernate/zadania/zadanie2.java
@@ -17,17 +17,20 @@ public class zadanie2 {
         int[] tablica = new int[] {1, 3, 5};
 
         StringBuilder builder = new StringBuilder("?");
+        String questionMark = ",?";
 
         for (int i=1; i<tablica.length; i++) {
-            builder.append(",?");
+            builder.append(questionMark);
         }
 
-        String query = "UPDATE product SET PRO_DESCRIPTION = CONCAT(PRO_DESCRIPTION, \" \", \"BRAK\") WHERE PRO_ID IN(" + builder.toString() +")";
+        String query = "UPDATE product SET PRO_DESCRIPTION = CONCAT(PRO_DESCRIPTION, ?) WHERE PRO_ID IN(" + builder.toString() +")";
 
         try (PreparedStatement statement = DatabaseConnection.getInstance().getConnection().prepareStatement(query)) {
 
+            statement.setString(1, " - BRAK");
+
             for (int i=0; i<tablica.length; i++) {
-                statement.setInt(i + 1, tablica[i]);
+                statement.setInt(i + 2, tablica[i]);
             }
 
             int countRecords = statement.executeUpdate();
